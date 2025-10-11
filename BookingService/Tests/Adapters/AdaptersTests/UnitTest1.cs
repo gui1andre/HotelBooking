cb7c93a5f670e0b13c2b6786f74e353d25b5680d
Guilherme Andre <gui1andre89@gmail.com>
2025-10-11 17:07:27 -0300

    Initial commit

---
+++ BookingService/Tests/Adapters/AdaptersTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace AdaptersTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ BookingService/Tests/Domain/DomainTests/Bookings/StateMachineTest.cs
@@ -0,0 +1,96 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+
+namespace DomainTests.Bookings;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ShouldAlwaysStartWithCreatedStatus()
+    {
+        var booking = new Booking();
+
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Created));
+    }
+
+    [Test]
+    public void ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Pay);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Paid));
+    }
+    
+    [Test]
+    public void ShouldSetStatusToCanceldWhenCacelingABookingWithCreatedStatus()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Cancel);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Canceled));
+    }
+    
+    [Test]
+    public void ShouldSetStatusToFinishedWhenFinishingAPaidBooking()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Pay);
+        booking.ChangeState(ActionEnum.Finish);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Finished));
+    }
+    
+    [Test]
+    public void ShouldSetStatusToRefoundedWhenRefoundingAPaidBOooking()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Pay);
+        booking.ChangeState(ActionEnum.Refound);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Refounded));
+    }
+    
+    [Test]
+    public void ShouldSetStatusToCreatedWhenReopingACanceledBooking()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Cancel);
+        booking.ChangeState(ActionEnum.Reopen);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Created));
+    }
+    
+    [Test]
+    public void ShouldNotChangeStatusWhenRefoundingABookingWithCreatedStatus()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Refound);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Created));
+    }
+    
+    [Test]
+    public void ShouldNotChangeStatusWhenRefoingAFinishedBooking()
+    {
+        var booking = new Booking();
+        
+        booking.ChangeState(ActionEnum.Pay);
+        booking.ChangeState(ActionEnum.Finish);
+        booking.ChangeState(ActionEnum.Refound);
+        
+        Assert.That(booking.CurrentStatus, Is.EqualTo(StatusEnum.Finished));
+        
+    }
+}
